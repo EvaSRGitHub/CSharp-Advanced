979ddf0ed64eb4aedd311416ff190a0c7d0c726b
Eva <eva_only@abv.bg>
2017-08-29 19:12:10 +0300

    CSharp Exams

---
+++ Hospital/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Patient
+{
+    public Patient(string departament, string doctor, string name, int room)
+    {
+        this.Departament = departament;
+        this.Doctor = doctor;
+        this.Name = name;
+        this.Room = room;
+    }
+
+    public string Departament{ get; }
+    public string Doctor { get; }
+    public string Name { get; }
+    public int Room { get; }
+    
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        List<Patient> patients = new List<Patient>();
+        Dictionary<string, int> departaments = new Dictionary<string, int>();
+
+        string input;
+        while ((input = Console.ReadLine())!="Output")
+        {
+            var tokens = input.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            
+            
+                string departament = tokens[0];
+                string doctor = tokens[1] + " " + tokens[2];
+                string patientName = tokens[3];
+
+                if (!departaments.ContainsKey(departament)) { departaments.Add(departament, 1); }
+                else
+                {
+                    if (departaments[departament] < 60)
+                    { departaments[departament] += 1; }
+                    else { continue; }
+                }
+
+
+                var currentRoom = (int)(Math.Ceiling(departaments[departament] / 3.0));
+
+                Patient patient = new Patient(departament, doctor, patientName, currentRoom);
+                patients.Add(patient);
+        }
+
+
+        while ((input = Console.ReadLine())!= "End")
+        {
+            var tokens = input.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (tokens.Length == 1)
+            {
+                var patientByDepartment = patients.GroupBy(x => new { x.Departament, x.Name }).Where(g => g.Key.Departament.Equals(tokens[0]));
+                    //.Select(g => new { Dep = g.Key.Departament, Name = g.Key.Name});
+
+                foreach (var item in patientByDepartment)
+                {
+                    Console.WriteLine($"{item.Key.Name}");
+                }
+            }
+            else
+            {
+                int room;
+                if (int.TryParse(tokens[1], out room))
+                {
+                    var patientByDepartmentAndRoom = patients.GroupBy(x => new { x.Departament, x.Name, x.Room }).Where(g => g.Key.Departament.Equals(tokens[0]) && g.Key.Room.Equals(int.Parse(tokens[1]))).Select(g => new { Dep = g.Key.Departament, Name = g.Key.Name, Room = g.Key.Room }).OrderBy(g => g.Name);
+
+                    foreach (var person in patientByDepartmentAndRoom)
+                    {
+                        Console.WriteLine($"{person.Name}");
+                    }
+                }
+                else // its doctor
+                {
+                    string doctor = tokens[0] + " " + tokens[1];
+                    var patientsByDoctor = patients.GroupBy(x => new
+                    {x.Doctor, x.Name}).Where(g => g.Key.Doctor.Equals(doctor)).Select(g => new { Doctor = g.Key.Doctor,Patients = g.Key.Name }).OrderBy( g => g.Patients);
+
+                    foreach (var person in patientsByDoctor)
+                    {
+                        Console.WriteLine($"{person.Patients}");
+                    }
+                }
+            }
+        }
+
+
+    }
+}
+
---
+++ Number Wars/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Card:IComparable<Card>
+{
+    public Card(int number, string letter)
+    {
+        this.Number = number;
+        this.Letter = letter;
+    }
+
+    public int Number { get; }
+    public string Letter { get; }
+
+    public int CompareTo(Card other)
+    {
+        var result = other.Number.CompareTo(this.Number);
+        if(result != 0) { return result; }
+        return other.Letter.CompareTo(this.Letter);
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Queue<Card> player1 = new Queue<Card>();
+        Queue<Card> player2 = new Queue<Card>();
+
+        var first = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+        ReadCards(player1, first);
+
+        var second = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+        ReadCards(player2, second);
+      
+
+        int turns = 0;
+        while (turns < 1000000 && (player1.Count > 0 && player2.Count > 0))
+        {
+            ++turns;
+            var currantCardPlayer1 = player1.Dequeue();
+            var currantCardPlayer2 = player2.Dequeue();
+
+            if (currantCardPlayer1.Number > currantCardPlayer2.Number)
+            {
+                player1.Enqueue(currantCardPlayer1);
+                player1.Enqueue(currantCardPlayer2);
+            }
+            else if (currantCardPlayer1.Number < currantCardPlayer2.Number)
+            {
+                player2.Enqueue(currantCardPlayer2);
+                player2.Enqueue(currantCardPlayer1);
+            }
+            else// war
+            {
+                bool hasWinner = false;
+                string alphabet = "abcdefghijklmnopqrstuvwxyz";
+                List<Card> warDack = new List<Card>();
+                warDack.Add(currantCardPlayer1);
+                warDack.Add(currantCardPlayer2);
+
+                while (hasWinner == false && (player1.Count > 0 && player2.Count > 0))
+                {
+                    var player1WarDack = player1.Take(3);
+                    warDack.AddRange(player1WarDack);
+                    var cards1 = player1WarDack.Select(i => i.Letter).ToArray();
+
+                    var player2WarDack = player2.Take(3);
+                    warDack.AddRange(player2WarDack);
+                    var cards2 = player2WarDack.Select(i => i.Letter).ToArray();
+
+                    int sum1 = 0, sum2 = 0;
+                    sum1 = GetSum(alphabet, cards1, sum1);
+                    sum2 = GetSum(alphabet, cards2, sum2);
+
+                    ThrowThreeOrLessCards(player1, cards1);
+                    ThrowThreeOrLessCards(player2, cards2);
+                    
+                    //check for winner
+                    if (sum1 != sum2)
+                    {
+                        warDack.Sort();
+                        var winner = sum1 > sum2 ? player1 : player2;
+                        for (int i = 0; i < warDack.Count(); i++)
+                        {
+                            winner.Enqueue(warDack[i]);
+                        }
+                        hasWinner = true;
+                    }
+                }
+            }
+        }
+
+        if (player1.Count != player2.Count)
+        {
+            Console.WriteLine("{0} player wins after {1} turns", player1.Count > player2.Count ? "First" : "Second", turns);
+        }
+        else { Console.WriteLine("Draw after {0} turns", turns); }
+
+    }
+
+    private static void ThrowThreeOrLessCards(Queue<Card> playerDack, string[] cards)
+    {
+        for (int i = 0; i < cards.Count(); i++)
+        {
+            playerDack.Dequeue();
+        }
+    }
+
+    private static int GetSum(string alphabet, string[] cards, int sum)
+    {
+        for (int i = 0; i < cards.Length; i++)
+        {
+            sum += (alphabet.IndexOf(cards[i])) + 1;
+        }
+
+        return sum;
+    }
+
+    private static void ReadCards(Queue<Card> playerDack, string[] first)
+    {
+        for (int i = 0; i < first.Length; i++)
+        {
+            int number = int.Parse(first[i].Substring(0, first[i].Length - 1));
+            string letter = (first[i].Last()).ToString();
+
+            playerDack.Enqueue(new Card(number, letter));
+        }
+    }
+}
+
